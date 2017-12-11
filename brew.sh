96181d8d2ed78d35aa442fd6ebf95b001867b21f
Riley Avron <ra.git@posteo.net>
2017-12-11 13:47:12 -0800

    Setup scripts from mathiasbynens

---
+++ brew.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -eux
+
+# Install command-line tools using Homebrew.
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Tap and install ripgrep
+brew tap burntsushi/ripgrep https://github.com/BurntSushi/ripgrep.git
+brew install burntsushi/ripgrep/ripgrep-bin
+
+# Install Bash 4.
+# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
+# running `chsh`.
+brew install bash
+brew install bash-completion2
+
+# Switch to using brew-installed bash as default shell
+if ! grep -q '/usr/local/bin/bash' /etc/shells; then
+  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+  chsh -s /usr/local/bin/bash;
+fi;
+
+# Install GnuPG to enable PGP-signing commits.
+brew install gnupg
+
+# Install more recent versions of some macOS tools.
+brew install vim --with-override-system-vi
+
+brew install git
+brew install cowsay
+brew install figlet
+brew install imagemagick
+brew install tree
+
+# TODO: Python3
+
+# Remove outdated versions from the cellar.
+brew cleanup
