0534f827aa9d1538b1e035e70ca1fc522cfe29ab
Riley Avron <riley.avron@gmail.com>
2019-07-14 16:30:39 -0700

    Add fzf to vim.sh

--- vim.sh
+++ vim.sh
@@ -62,6 +62,8 @@ package git@github.com:xolox/vim-misc.git
 package git@github.com:morhetz/gruvbox.git
 package git@github.com:leafgarland/typescript-vim.git
 package git@github.com:cespare/vim-toml.git
+package git@github.com:junegunn/fzf.git
+package git@github.com:junegunn/fzf.vim.git
 
 # Wait for all outstanding jobs to terminate. Only necessary if running jobs in
 # background.
