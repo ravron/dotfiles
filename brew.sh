48929a79816ea02a7fef6a717a6bb03f1060bc9a
Riley Avron <ra.git@posteo.net>
2022-08-31 10:21:53 -0700

    Update Homebrew install command

--- brew.sh
+++ brew.sh
@@ -9,7 +9,7 @@ if ! command -v brew &> /dev/null; then
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         echo 'Installing Homebrew...'
         # See https://brew.sh/
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     else
         echo 'You can install it manually from https://brew.sh/. Exiting.'
         exit 1
